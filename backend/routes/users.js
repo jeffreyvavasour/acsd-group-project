230dc25b3c8d67e41a4f25aa20fe972426227791
Paweł Wierzgoń <pawel.wierzgon@gmail.com>
2024-04-20 23:22:13 +0100

    Clear the backend app

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 var sqlite = require("better-sqlite3");
-var bcrypt = require('bcrypt');
-
+var bcrypt = require("bcrypt");
 
 /**
  * @openapi
@@ -20,25 +19,25 @@ var bcrypt = require('bcrypt');
  *       401:
  *         description: Returns message saying token has expired due to invalid JWT token
  */
-router.get('/user', async function (req, res, next) {
-  var token = req.query.token
+router.get("/user", async function (req, res, next) {
+  var token = req.query.token;
 
   try {
     var decoded = await jwt.verify(token, "SECRET_KEY", { complete: true });
   } catch (error) {
-    console.log(error)
-    return res.status(401).json({ "message": "Token expired" })
+    console.log(error);
+    return res.status(401).json({ message: "Token expired" });
   }
   if (bcrypt.compareSync(decoded.payload.user_id, decoded.payload.hash)) {
-    var db = new sqlite('database.db');
-    var user = await db.prepare('SELECT * FROM users where id = (?)').get(decoded.payload.user_id);
-    return res.json({ username: user.username })
+    var db = new sqlite("database.db");
+    var user = await db
+      .prepare("SELECT * FROM users where id = (?)")
+      .get(decoded.payload.user_id);
+    return res.json({ username: user.username });
   } else {
-    return res.status(401).json({ "message": "Token expired" })
+    return res.status(401).json({ message: "Token expired" });
   }
-
 });
-
 
 /**
  * @openapi
@@ -55,32 +54,40 @@ router.get('/user', async function (req, res, next) {
  *       400:
  *         description: Returns invalid credentials when wrong credentials provided
  */
-router.post('/login', async function (req, res) {
+router.post("/login", async function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
 
   try {
-    var db = new sqlite('database.db');
-    var user = await db.prepare('SELECT * FROM users where username = (?)').get(username);
+    var db = new sqlite("database.db");
+    var user = await db
+      .prepare("SELECT * FROM users where username = (?)")
+      .get(username);
     if (user && bcrypt.compareSync(password + "", user.password)) {
-      var token = jwt.sign({ user_id: user.id + "", hash: await bcrypt.hashSync(user.id + "", 10) }, "SECRET_KEY", { expiresIn: '7d' })
+      var token = jwt.sign(
+        {
+          user_id: user.id + "",
+          hash: await bcrypt.hashSync(user.id + "", 10),
+        },
+        "SECRET_KEY",
+        { expiresIn: "7d" }
+      );
       return res.json({
-        token: token
-      })
+        token: token,
+      });
     } else {
       return res.status(400).json({
-        message: "Invalid credentials"
-      })
+        message: "Invalid credentials",
+      });
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 
   res.status(400).json({
-    message: "Invalid credentials"
-  })
+    message: "Invalid credentials",
+  });
 });
-
 
 /**
  * @openapi
@@ -97,26 +104,31 @@ router.post('/login', async function (req, res) {
  *       400:
  *         description: Return user already exists when user already exists in system
  */
-router.post('/register', async function (req, res) {
+router.post("/register", async function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
   try {
-    var db = new sqlite('database.db');
-    var users = await db.prepare('SELECT * FROM users WHERE username = (?)').all(username);
+    var db = new sqlite("database.db");
+    var users = await db
+      .prepare("SELECT * FROM users WHERE username = (?)")
+      .all(username);
     if (users.length) {
       return res.status(400).json({
-        message: "This username already exists"
-      })
+        message: "This username already exists",
+      });
     }
-    var hash = await bcrypt.hashSync(password + '', 10);
-    db.prepare('INSERT INTO users (username, password) VALUES (?, ?)').run(username, hash);
-    return res.status(201).json({})
+    var hash = await bcrypt.hashSync(password + "", 10);
+    db.prepare("INSERT INTO users (username, password) VALUES (?, ?)").run(
+      username,
+      hash
+    );
+    return res.status(201).json({});
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
   return res.status(400).json({
-    message: "An error occured, please try again later"
-  })
+    message: "An error occured, please try again later",
+  });
 });
 
 module.exports = router;
